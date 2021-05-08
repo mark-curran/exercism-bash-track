23d9edc5fa7e09ed76c01eb46ebdae2ea8671202
mark-curran <mjcur_11@hotmail.com>
2021-05-08 17:41:25 +0200

    All tests passing.

--- bash/hamming/hamming.sh
+++ bash/hamming/hamming.sh
@@ -1,49 +1,39 @@
 #!/usr/bin/env bash
 
-checkvalidchar(){
-    local validchars=(G A T C)
-    local invalid=0
-    for char in ${validchars[*]}; do
-        if [[ $char = $1 ]]; then
-            invalid=1
-        fi
-    done
-    if (( $invalid == 0 )); then
-        echo "Invalid character in string."
-        exit 1
-    fi
-}
+# Exit with error code 1 if the input is empty
+if ! (( $# == 2)); then
+    echo "Usage: hamming.sh <string1> <string2>"
+    exit 1
+fi
+# Return hamming distance 0 for identical empty inputs
+if ((${#1} == 0 )) && ((${#2} == 0)); then
+    echo 0
+    exit 0
+fi
+# Test no input right
+if ((${#1} == 0)); then
+    echo "left and right strands must be of equal length"
+    exit 1
+fi
+# Test no input left
+if ((${#2} == 0)); then
+    echo "left and right strands must be of equal length"
+    exit 1
+fi
+# Test inputs equal length
+if ! (( ${#1} == ${#2} )); then
+    echo "left and right strands must be of equal length"
+    exit 1
+fi
 
-main() {
-    string1=$1
-    string2=$2
-    # Exit with error code 1 if the input is empty
-    if (( $# == 0)); then
-        echo "Usage: hamming.sh <string1> <string2>"
-        exit 1
-    fi
-    # Return hamming distance 0 for identical empty inputs
-    if ((${#string1} == 0 )) && ((${#string2} == 0)); then
-        echo 0
-        exit 0
-    fi
-    # Test no input
-    if ((${#string1} == 0)); then
-        echo "Usage: hamming.sh <string1> <string2>"
-        exit 1
-    fi
-    # Test inputs equal length
-    if ! (( ${#string1} == ${#string2} )); then
-        echo "left and right strands must be of equal length"
-        exit 1
-    fi
+hammingdist(){
     local x=0
+    local string1=$1
+    local string2=$2
     for ((i=0; i<${#string1}; i++)) do
         local char1=${string1:i:1}
         local char2=${string2:i:1}
-        checkvalidchar $char1
-        checkvalidchar $char2
-        if ! [[ $char1 == $char2 ]]; then
+        if ! [[ "$char1" == "$char2" ]]; then
             ((x++))
         fi
     done
@@ -51,4 +41,4 @@ main() {
     exit 0
 }
 
-main $1 $2
+hammingdist $1 $2
