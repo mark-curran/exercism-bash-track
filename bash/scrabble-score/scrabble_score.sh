31d689f6ef549f6ceeb33e42adf7df6f5f6b1c01
mark-curran <mjcur_11@hotmail.com>
2021-12-13 16:05:05 +1100

    Fix scrabble-score spellcheck complaints.

--- bash/scrabble-score/scrabble_score.sh
+++ bash/scrabble-score/scrabble_score.sh
@@ -5,60 +5,62 @@ declare -A values
 
 # Add letters with value equal to 1
 letters=("A" "E" "I" "O" "U" "L" "N" "R" "S" "T")
-for letter in ${letters[@]}; do
+for letter in "${letters[@]}"; do
     values[$letter]=1
 done
 
 # Add letters with value equal to 2
 letters=("D" "G")
-for letter in ${letters[@]}; do
+for letter in "${letters[@]}"; do
     values[$letter]=2
 done
 
 # Add letters with value equal to 2
 letters=("D" "G")
-for letter in ${letters[@]}; do
+for letter in "${letters[@]}"; do
     values[$letter]=2
 done
 
 # Add letters with value equal to 3
 letters=("B" "C" "M" "P")
-for letter in ${letters[@]}; do
+for letter in "${letters[@]}"; do
     values[$letter]=3
 done
 
 # Add letters with value equal to 4
 letters=("F" "H" "V" "W" "Y")
-for letter in ${letters[@]}; do
+for letter in "${letters[@]}"; do
     values[$letter]=4
 done
 
 # Add letters with value equal to 5
 letters=("K")
-for letter in ${letters[@]}; do
+for letter in "${letters[@]}"; do
     values[$letter]=5
 done
 
 # Add letters with value equal to 8
 letters=("J" "X")
-for letter in ${letters[@]}; do
+for letter in "${letters[@]}"; do
     values[$letter]=8
 done
 
 # Add letters with value equal to 10
 letters=("Q" "Z")
-for letter in ${letters[@]}; do
+for letter in "${letters[@]}"; do
     values[$letter]=10
 done
 
 main () {
-    local uppercase_input="$( echo $1 | tr [a-z] [A-Z])"
+    local uppercase_input
+    uppercase_input="$( echo "$1" | tr '[:lower:]' '[:upper:]')"
+
     local score=0
 
     for ((i=0; i<${#uppercase_input}; i++)); do
         letter=${uppercase_input:i:1}
         value=${values[$letter]}
-        ((score+=$value))
+        ((score += value))
     done
     echo $score
 }
