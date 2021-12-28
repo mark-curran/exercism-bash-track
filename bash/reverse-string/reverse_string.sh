4ddae091ab8669dbb0caa7618299012f55b1d058
mark-curran <mjcur_11@hotmail.com>
2021-12-28 15:01:33 +1100

    Spellcheck Checks passing.

--- bash/reverse-string/reverse_string.sh
+++ bash/reverse-string/reverse_string.sh
@@ -5,8 +5,8 @@ main() {
     local output_string=''
 
     # Iterate over characters in first argument.
-    for ((i=0; i<$length_of_input; i++)); do
-        letter=${1:$((length_of_input-i-1)):1}
+    for ((i=0; i<length_of_input; i++)); do
+        letter=${1:((length_of_input-i-1)):1}
         output_string+="${letter}"
     done
 
