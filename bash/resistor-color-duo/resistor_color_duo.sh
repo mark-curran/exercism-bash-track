b3cb13862ccd63e85a42bf0fad47b833f9459227
mark-curran <mjcur_11@hotmail.com>
2021-12-30 11:50:39 +1100

    Solution with all tests passing.

--- bash/resistor-color-duo/resistor_color_duo.sh
+++ bash/resistor-color-duo/resistor_color_duo.sh
@@ -1,24 +1,21 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+# A list of the colors, ordered by their resistance value
+colors=('black' 'brown' 'red' 'orange' 'yellow' 'green' 'blue' 'violet' 'grey' 'white')
+
+# Check the first two value of the input are in the list
+for arg in "${@:1:2}"; do
+    if [[ $( echo "${colors[@]}" | grep -w -q "$arg" && echo '' || echo ' ' ) ]]; then
+        echo "$arg is an invalid color"
+        exit 1 
+    fi
+done
+
+# Create an array with the value of each color
+declare -A color_values
+for ((i=0; i<${#colors[@]}; i++)); do
+    color_values["${colors[i]}"]=$i
+done
+
+# Return the values of the first and second output, drop any leading zeroes
+echo "$( echo "${color_values["$1"]}" | tr -d '0' )""${color_values["$2"]}"
