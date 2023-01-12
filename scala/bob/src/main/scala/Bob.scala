b67111f068cf3f9b10d99aebd434797edf4d2212
root <mark.curran@pepperstone.com>
2023-01-12 22:44:24 +0000

    Initial solution to bob exercise.

--- scala/bob/src/main/scala/Bob.scala
+++ scala/bob/src/main/scala/Bob.scala
@@ -1,3 +1,16 @@
 object Bob {
-  def response(statement: String): String = ???
+  def response(statement: String): String = {
+    
+    val is_question: Boolean = statement.takeRight(1) == "?"
+    val is_uppercase: Boolean = statement == statement.toUpperCase()
+
+    if (is_question) { 
+      "Sure."
+    } else if (is_uppercase) {
+      "Whoa, chill out!"
+    } else {
+      "Whatever."
+    }
+
+  }
 }
